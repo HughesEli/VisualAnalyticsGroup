ab9451d6d868530a7816beb3f7a448cffe4aec58
HughesEli <hugheseb@appstate.edu>
2024-04-24 19:31:39 -0400

    try this

--- NBA_piechart.js
+++ NBA_piechart.js
@@ -1,3 +1,5 @@
+import * as d3 from 'd3';
+
 function createPieChart(width, height) {
     const size = {
         width: width,
@@ -8,6 +10,15 @@ function createPieChart(width, height) {
     const svg = d3.select('#piechart-svg');
 
     d3.csv("injuries_2010-2020.csv").then(function(data) {
+        console.log(data); // Log the loaded data to check if it's correct
+
+        const injuryCounts = d3.group()
+            .key(d => d.Relinquished)
+            .rollup(v => v.length)
+            .entries(data);
+
+        console.log(injuryCounts); // Log the aggregated data
+
         const pie = d3.pie()
             .value(d => d.value)
             .sort(null);
@@ -16,20 +27,19 @@ function createPieChart(width, height) {
             .innerRadius(0)
             .outerRadius(Math.min(size.width, size.height) / 2 - size.margin);
 
-        const arcs = pie(data);
+        const arcs = pie(injuryCounts);
 
         const colorScale = d3.scaleOrdinal()
-            .domain(data.map(d => d.category))
+            .domain(injuryCounts.map(d => d.key))
             .range(d3.schemeCategory10);
 
         svg.selectAll("path")
             .data(arcs)
             .enter().append("path")
             .attr("d", arc)
-            .attr("fill", d => colorScale(d.data.category))
+            .attr("fill", d => colorScale(d.data.key))
             .attr("transform", `translate(${size.width / 2}, ${size.height / 2})`);
 
-    
         const labelArc = d3.arc()
             .outerRadius(Math.min(size.width, size.height) / 2 - size.margin)
             .innerRadius(0);
@@ -39,7 +49,7 @@ function createPieChart(width, height) {
             .enter().append("text")
             .attr("transform", d => `translate(${labelArc.centroid(d)})`)
             .attr("dy", "0.35em")
-            .text(d => d.data.category);
+            .text(d => d.data.key);
 
         svg.append("text")
             .attr("x", size.width / 2)
